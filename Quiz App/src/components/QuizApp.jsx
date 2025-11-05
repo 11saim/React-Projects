dcbad1d74ca4b0e8f8d752f35e98d158a75e4da7
Saim Shabbir <shabbirsaim333@gmail.com>
2025-11-05 22:46:21 +0500

    Button Added

--- Quiz App/src/components/QuizApp.jsx
+++ Quiz App/src/components/QuizApp.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Button from "./Button";
 
 function QuizApp() {
   return (
     <div className="bg-[#430d97] w-96 rounded-3xl m-2 py-4">
       <Header />
       <Main />
+      <Button />
     </div>
   );
 }
