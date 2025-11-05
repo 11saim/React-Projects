9f10bfd3aa53c4f8d60245418caf8472e26452f2
Saim Shabbir <shabbirsaim333@gmail.com>
2025-11-05 21:48:43 +0500

    Timer Component Added

--- Quiz App/src/components/Header.jsx
+++ Quiz App/src/components/Header.jsx
@@ -1,17 +1,14 @@
 import React from "react";
-import stopwatch from "../assets/stopwatch.png";
+import Timer from "./Timer";
 
 export default function Header() {
   return (
-    <div className="flex my-4 flex-col my-6 space-y-6">
+    <div className="flex my-4 flex-col space-y-6">
       <div className="flex justify-around items-center">
         <p className="text-white font-bold">
           {0} of {10}
         </p>
-        <div className="w-30 rounded-[50px] px-3 py-1 bg-[#8538f8] flex justify-between items-center">
-          <img className="w-8 h-8" src={stopwatch} alt="Stop-Watch" />
-          <p className="text-white text">03:58</p>
-        </div>
+        <Timer />
       </div>
       <div className="progress-bar">
         <div className="w-[90%] h-4 bg-black m-auto rounded-[40px]">
