93f0721fa482f035dcdaa398fdc5e0fcd35e1434
Saim Shabbir <shabbirsaim333@gmail.com>
2025-11-06 22:22:00 +0500

    Style Changes

--- Quiz App/src/components/Button.jsx
+++ Quiz App/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Button() {
   return (
-    <div className="relative w-40 mx-auto mt-12 cursor-pointer text-xl text-center">
+    <div className="relative w-40 mx-auto mt-5 xs:mt-12 cursor-pointer text-xl text-center">
       <div className="absolute w-full h-full bg-green-700 rounded-2xl top-1 left-0 z-10"></div>
       <div className="relative z-20 bg-green-400 text-white p-4 rounded-2xl font-bold">
         Next
