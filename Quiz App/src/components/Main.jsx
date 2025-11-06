604d1105852603525a33d6c928a99f1df1897b60
Saim Shabbir <shabbirsaim333@gmail.com>
2025-11-06 22:31:45 +0500

    Data Added

--- Quiz App/src/components/Main.jsx
+++ Quiz App/src/components/Main.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function Main() {
   return (
     <div className="relative m-4 mt-7 xs:m-5">
-      <div className="absolute top-0 xs:top-6 left-[10%] w-[80%] h-full bg-[#9f61f7] rounded-3xl z-10"></div>
-      <div className="absolute top-0 xs:top-3 left-[5%] w-[90%] h-full bg-[#c99bfe] rounded-3xl z-20"></div>
+      <div className="absolute top-0 xs:top-4 left-[10%] w-[80%] h-full bg-[#9f61f7] rounded-3xl z-10"></div>
+      <div className="absolute top-0 xs:top-2 left-[5%] w-[90%] h-full bg-[#c99bfe] rounded-3xl z-20"></div>
       <div className="bg-white p-5 xs:p-6 rounded-3xl relative z-30">
         <div className="question">
           <p className="text-lg xs:text-xl font-bold leading-7">
