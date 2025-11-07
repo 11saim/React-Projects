f9a3116e59cc93256ba4e3dc7cc03019da3413a3
Saim Shabbir <shabbirsaim333@gmail.com>
2025-11-07 22:41:44 +0500

    Total Question & Progress Bar Are Dynamic

--- Quiz App/src/components/Header.jsx
+++ Quiz App/src/components/Header.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import Timer from "./Timer";
 
-export default function Header() {
+export default function Header({ currQues, totalQues }) {
   return (
     <div className="flex my-4 flex-col space-y-6">
       <div className="flex justify-around items-center">
         <p className="text-white font-bold">
-          {0} of {10}
+          {currQues} of {totalQues}
         </p>
         <Timer />
       </div>
       <div className="progress-bar">
         <div className="w-[90%] h-4 bg-black m-auto rounded-[40px]">
-          <div className="w-[50%] h-4 bg-[#3ab77d] rounded-[40px]"></div>
+          <div
+            className="h-4 bg-[#3ab77d] rounded-[40px] transition-all"
+            style={{ width: `${(currQues / totalQues) * 100}%` }}
+          ></div>
         </div>
       </div>
     </div>
--- Quiz App/src/components/QuizApp.jsx
+++ Quiz App/src/components/QuizApp.jsx
@@ -6,12 +6,13 @@ import { useEffect, useState } from "react";
 
 function QuizApp() {
   const [data, setData] = useState(null);
+  const [currQues, setcurrQues] = useState(1);
   useEffect(() => {
     async function fetchData() {
       try {
         const response = await fetch("../../public/data.json");
         const jsonData = await response.json();
-        setData(jsonData);
+        setData([...jsonData]);
       } catch (error) {
         console.error("Failed to fetch data:", error);
       }
@@ -21,7 +22,7 @@ function QuizApp() {
 
   return data ? (
     <div className="bg-[#430d97] w-96 rounded-3xl m-2 py-4">
-      <Header />
+      <Header currQues={currQues} totalQues={data.length} />
       <Main />
       <Button />
     </div>
