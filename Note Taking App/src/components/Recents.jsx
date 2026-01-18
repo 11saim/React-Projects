458a8778e7ae7379486e95c9caeadf6ac7baeea4
Saim Shabbir <shabbirsaim333@gmail.com>
2026-01-18 21:43:16 +0500

    Recents Styled

--- Note Taking App/src/components/Recents.jsx
+++ Note Taking App/src/components/Recents.jsx
@@ -1,30 +1,31 @@
 import React from "react";
-import noteIcon from "../assets/note-icon.png";
+import whiteNoteIcon from "../assets/white-note-icon.png";
+import greyNoteIcon from "../assets/grey-note-icon.png";
 
 export default function Recents() {
   return (
-    <div className="recents-section">
-      <div className="head">
+    <div className="recents-section py-7 text-[#a3a3a3]">
+      <div className="head px-3 py-2">
         <div className="heading">
           <h4>Recents</h4>
         </div>
       </div>
       <div className="notes">
-        <div className="note">
+        <div className="note flex p-3 space-x-3 items-center bg-[#312EB5] text-white">
           <div className="note-icon">
-            <img src={noteIcon} alt="note-icon" />
+            <img src={whiteNoteIcon} alt="note-icon" width={20} height={20} />
           </div>
           <div className="note-title">Reflection on the Month of June</div>
         </div>
-        <div className="note">
+        <div className="note flex p-3 space-x-2 items-center">
           <div className="note-icon">
-            <img src={noteIcon} alt="note-icon" />
+            <img src={greyNoteIcon} alt="note-icon" width={20} height={20} />
           </div>
-          <div className="note-title">Project proposal</div>
+          <div className="note-title]">Project proposal</div>
         </div>
-        <div className="note">
+        <div className="note flex p-3 space-x-2 items-center">
           <div className="note-icon">
-            <img src={noteIcon} alt="note-icon" />
+            <img src={greyNoteIcon} alt="note-icon" width={20} height={20} />
           </div>
           <div className="note-title">Travel itinerary</div>
         </div>
