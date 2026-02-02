0e88305aea76416baee8a584c9c9d8b865e3bc4c
Saim Shabbir <shabbirsaim333@gmail.com>
2026-02-02 22:34:40 +0500

    Toggle Hanldes

--- Note Taking App/src/components/NoteHead.jsx
+++ Note Taking App/src/components/NoteHead.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import closedOptions from "../assets/closed-options.png";
 import favoriteIcon from "../assets/favorite-icon.png";
 import archivedIcon from "../assets/archived-icon.png";
 import trashIcon from "../assets/trash-icon.png";
 
 export default function NoteHead() {
+  const [isModel, setIsModel] = useState(false);
+
   return (
     <div className="note-head flex justify-between items-end">
       <div className="note-title">
@@ -12,36 +14,47 @@ export default function NoteHead() {
           Reflection on the Month of June
         </p>
       </div>
+
       <div className="options relative">
-        <img src={closedOptions} alt="closed-options" width={40} height={40} />
-        <div className="options-modal bg-[#333333] p-4 rounded-md hidden top-13 right-1 w-48 space-y-5">
-          <div className="top-options space-y-4 border-b-2 border-b-[#3d3d3d]">
-            <div className="add-to-favorite flex space-x-2">
-              <img
-                src={favoriteIcon}
-                alt="favorite-icon"
-                width={25}
-                height={25}
-              />
-              <p>Add to Favorites</p>
+        <img
+          onClick={() => setIsModel(!isModel)}
+          src={closedOptions}
+          alt="closed-options"
+          width={40}
+          height={40}
+          className="cursor-pointer"
+        />
+
+        {isModel && (
+          <div className="options-modal bg-[#333333] p-4 rounded-md absolute top-13 right-1 w-48 space-y-5">
+            <div className="top-options space-y-4 border-b-2 border-b-[#3d3d3d]">
+              <div className="add-to-favorite flex space-x-2 cursor-pointer">
+                <img
+                  src={favoriteIcon}
+                  alt="favorite-icon"
+                  width={25}
+                  height={25}
+                />
+                <p>Add to Favorites</p>
+              </div>
+              <div className="archived flex space-x-2 mb-2 cursor-pointer">
+                <img
+                  src={archivedIcon}
+                  alt="archived-icon"
+                  width={25}
+                  height={25}
+                />
+                <p>Archived</p>
+              </div>
             </div>
-            <div className="archived flex space-x-2 mb-2">
-              <img
-                src={archivedIcon}
-                alt="archived-icon"
-                width={25}
-                height={25}
-              />
-              <p>Archived</p>
+            <div className="bottom-options">
+              <div className="delete flex space-x-2 cursor-pointer">
+                <img src={trashIcon} alt="trash-icon" width={25} height={25} />
+                <p>Delete</p>
+              </div>
             </div>
           </div>
-          <div className="bottom-options">
-            <div className="delete flex space-x-2">
-              <img src={trashIcon} alt="trash-icon" width={25} height={25} />
-              <p>Delete</p>
-            </div>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
--- Note Taking App/src/components/TextOptions.jsx
+++ Note Taking App/src/components/TextOptions.jsx
@@ -1,45 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import arrow from "../assets/arrow.png";
 
 export default function TextOptions() {
+  const [isTextSelect, setIsTextSelect] = useState(false);
+  const [isOptionSelect, setIsOptionSelect] = useState(false);
   return (
     <div className="text-options flex space-x-7 lg:space-x-3">
       <div className="select text-select relative">
-        <div className="selected-option flex space-x-4 lg:space-x-9">
+        <div
+          onClick={() => setIsTextSelect(!isTextSelect)}
+          className="selected-option flex space-x-4 lg:space-x-9 cursor-pointer"
+        >
           <p>Paragraph</p>
           <img src={arrow} alt="arrow" width={25} height={25} />
         </div>
-        <ul className="options bg-[#1f1f1f] mt-3 hidden w-32">
-          <li className="hover:bg-[#312EB5] p-2">Paragraph</li>
-          <li className="hover:bg-[#312EB5] p-2">Quote</li>
-          <li className="hover:bg-[#312EB5] p-2">Code block</li>
-          <li className="hover:bg-[#312EB5] p-2">Bullet list</li>
-          <li className="hover:bg-[#312EB5] p-2">Numbered list</li>
-          <li className="hover:bg-[#312EB5] p-2">Checklist</li>
-        </ul>
+        {isTextSelect && (
+          <ul className="options bg-[#1f1f1f] mt-3 absolute w-32">
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">Paragraph</li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">Quote</li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">
+              Code block
+            </li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">
+              Bullet list
+            </li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">
+              Numbered list
+            </li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">Checklist</li>
+          </ul>
+        )}
       </div>
       <div className="select option-select relative">
-        <div className="selected-option flex space-x-5">
+        <div
+          onClick={() => setIsOptionSelect(!isOptionSelect)}
+          className="selected-option flex space-x-5 cursor-pointer"
+        >
           <p>16</p>
           <img src={arrow} alt="arrow" width={25} height={25} />
         </div>
-        <ul className="options bg-[#1f1f1f] mt-3 hidden w-15">
-          <li className="hover:bg-[#312EB5] p-2">8</li>
-          <li className="hover:bg-[#312EB5] p-2">9</li>
-          <li className="hover:bg-[#312EB5] p-2">10</li>
-          <li className="hover:bg-[#312EB5] p-2">11</li>
-          <li className="hover:bg-[#312EB5] p-2">12</li>
-          <li className="hover:bg-[#312EB5] p-2">14</li>
-          <li className="hover:bg-[#312EB5] p-2">16</li>
-          <li className="hover:bg-[#312EB5] p-2">18</li>
-          <li className="hover:bg-[#312EB5] p-2">20</li>
-          <li className="hover:bg-[#312EB5] p-2">24</li>
-          <li className="hover:bg-[#312EB5] p-2">28</li>
-          <li className="hover:bg-[#312EB5] p-2">32</li>
-          <li className="hover:bg-[#312EB5] p-2">36</li>
-          <li className="hover:bg-[#312EB5] p-2">48</li>
-          <li className="hover:bg-[#312EB5] p-2">72</li>
-        </ul>
+        {isOptionSelect && (
+          <ul className="options bg-[#1f1f1f] mt-3 absolute w-15">
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">8</li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">9</li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">10</li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">11</li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">12</li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">14</li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">16</li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">18</li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">20</li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">24</li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">28</li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">32</li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">36</li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">48</li>
+            <li className="hover:bg-[#312EB5] p-2 cursor-pointer">72</li>
+          </ul>
+        )}
       </div>
     </div>
   );
