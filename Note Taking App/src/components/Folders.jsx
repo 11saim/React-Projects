fae083930a35d6d9119026aecde7fe948a9d3313
Saim Shabbir <shabbirsaim333@gmail.com>
2026-02-08 23:02:54 +0500

    Add Folder Modal

--- Note Taking App/src/components/Folders.jsx
+++ Note Taking App/src/components/Folders.jsx
@@ -1,76 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import openFolderIcon from "../assets/open-folder-icon.png";
 import closeFolderIcon from "../assets/close-folder-icon.png";
 import addFolderIcon from "../assets/add-folder-icon.png";
 
 export default function Folders() {
+  const [isModal, setIsModal] = useState(false);
   return (
-    <div className="folders-section py-3 text-[#a3a3a3]">
-      <div className="head flex justify-between px-3 py-2">
-        <div className="heading">
-          <h4>Folders</h4>
+    <>
+      <div className="folders-section py-3 text-[#a3a3a3]">
+        <div className="head flex justify-between px-3 py-2">
+          <div className="heading">
+            <h4>Folders</h4>
+          </div>
+          <div
+            onClick={() => setIsModal(true)}
+            className="icon hover:scale-120 transition-all cursor-pointer"
+          >
+            <img src={addFolderIcon} alt="add-folder" />
+          </div>
         </div>
-        <div className="icon">
-          <img src={addFolderIcon} alt="add-folder" />
+        <div className="folders">
+          <div className="folder flex p-3 space-x-3 items-center bg-[#1f1f1f] text-white">
+            <div className="folder-icon">
+              <img
+                src={openFolderIcon}
+                alt="folder-icon"
+                width={20}
+                height={20}
+              />
+            </div>
+            <div className="folder-title">Personal</div>
+          </div>
+          <div className="folder flex p-3 space-x-3 items-center hover:bg-[#1f1f1f]">
+            <div className="folder-icon">
+              <img
+                src={closeFolderIcon}
+                alt="folder-icon"
+                width={20}
+                height={20}
+              />
+            </div>
+            <div className="folder-title">Work</div>
+          </div>
         </div>
       </div>
-      <div className="folders">
-        <div className="folder flex p-3 space-x-3 items-center bg-[#1f1f1f] text-white">
-          <div className="folder-icon">
-            <img
-              src={openFolderIcon}
-              alt="folder-icon"
-              width={20}
-              height={20}
-            />
+      {isModal && (
+        <div class="fixed inset-0 z-50 flex items-center justify-center bg-white/10">
+          <div class="relative w-96 bg-[#181818] p-3 sm:p-4 rounded-2xl mx-3">
+            <div class="flex items-center justify-between">
+              <h2 class="text-xl font-semibold">Folder Name:</h2>
+
+              <button
+                class="text-gray-500 cursor-pointer text-2xl font-bold"
+                aria-label="Close"
+                onClick={() => setIsModal(false)}
+              >
+                ×
+              </button>
+            </div>
+
+            <div class="py-4">
+              <input
+                className="bg-[#232323] w-full px-5 py-3 outline-0"
+                type="text"
+                name="foldername"
+                placeholder="Enter Here"
+              />
+            </div>
+
+            <div class="w-full flex justify-end">
+              <button class="px-2 py-1 sm:px-4 sm:py-2 rounded bg-blue-600 text-white hover:bg-blue-700 cursor-pointer">
+                Save
+              </button>
+            </div>
           </div>
-          <div className="folder-title">Personal</div>
         </div>
-        <div className="folder flex p-3 space-x-3 items-center">
-          <div className="folder-icon">
-            <img
-              src={closeFolderIcon}
-              alt="folder-icon"
-              width={20}
-              height={20}
-            />
-          </div>
-          <div className="folder-title">Work</div>
-        </div>
-        <div className="folder flex p-3 space-x-3 items-center">
-          <div className="folder-icon">
-            <img
-              src={closeFolderIcon}
-              alt="folder-icon"
-              width={20}
-              height={20}
-            />
-          </div>
-          <div className="folder-title">Travel</div>
-        </div>
-        <div className="folder flex p-3 space-x-3 items-center">
-          <div className="folder-icon">
-            <img
-              src={closeFolderIcon}
-              alt="folder-icon"
-              width={20}
-              height={20}
-            />
-          </div>
-          <div className="folder-title">Events</div>
-        </div>
-        <div className="folder flex p-3 space-x-3 items-center">
-          <div className="folder-icon">
-            <img
-              src={closeFolderIcon}
-              alt="folder-icon"
-              width={20}
-              height={20}
-            />
-          </div>
-          <div className="folder-title">Finances</div>
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 }
